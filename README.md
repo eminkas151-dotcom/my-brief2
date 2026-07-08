8c78768c1eeae61bc862fa8aef76f34bd960d270
eminkas151-dotcom <eminkas151@gmail.com>
2026-07-08 17:27:30 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# My Brief v0.4
-
-This version separates content from the interface.
-
-Files:
-- index.html
-- styles.css
-- app.js
-- manifest.json
-- service-worker.js
-- icon.svg
-- data/briefing.json
-
-To update the briefing later, edit only:
-data/briefing.json
+v0.4.1: briefing.json moved to project root.
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,1 @@
-const CACHE = "my-brief-v0-4";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg", "./data/briefing.json"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+self.addEventListener('fetch',e=>{});
